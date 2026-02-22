86cc9aa4d69e6447a0496bb9308bdab8985937b7
EmreHaksever <emre.haksever2@gmail.com>
2026-02-22 22:53:55 +0300

    db ayağa kaldırıldı.

--- MiniERP.API/Program.cs
+++ MiniERP.API/Program.cs
@@ -1,15 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using MiniERP.Application.Interfaces;
+using MiniERP.Application.Interfaces.Repositories;
+using MiniERP.Infrastructure.Context;
+using MiniERP.Infrastructure.Repositories;
+using MiniERP.Infrastructure.UnitOfWork;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 1. Veritabaný Baðlantýsýný (DbContext) Sisteme Tanýtýyoruz
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// 2. DEPENDENCY INJECTION (Baðýmlýlýk Enjeksiyonu) Ayarlarý
+// "AddScoped" ne demek? (Mülakat Sorusu!): Her yeni HTTP isteði (Request) geldiðinde bu sýnýftan 1 tane yeni nesne üret, 
+// istek bittiðinde (Response dönünce) nesneyi çöpe at demektir. Web projeleri için en güvenli yöntemdir.
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
+// UnitOfWork'ü sisteme tanýtýyoruz
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +36,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
